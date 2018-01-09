03cadcc4ad679c8bde80755c9fb8d9ad89079fdb
Aiden Banuelos <coronadoac16@gmail.com>
2018-01-09 16:46:27 -0700

    Initial commit. Blank Robot Template

---
+++ src/Robot.hpp
@@ -0,0 +1,35 @@
+/*
+ * Robot.hpp
+ *
+ *  Created on: Jan 9, 2018
+ *      Author: Local Admin
+ */
+
+#include <memory>
+
+#include <Commands/Command.h>
+#include <Commands/Scheduler.h>
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+
+#include "Commands/ExampleCommand.h"
+#include "CommandBase.h"
+
+class Robot: public frc::IterativeRobot {
+public:
+	void RobotInit() override;
+	void DisabledInit() override;
+	void DisabledPeriodic() override;
+	void AutonomousInit() override;
+	void AutonomousPeriodic() override;
+	void TeleopInit() override;
+	void TeleopPeriodic() override;
+	void TestPeriodic() override;
+private:
+	std::unique_ptr<frc::Command> autonomousCommand;
+	frc::SendableChooser<frc::Command*> chooser;
+};
+
+
