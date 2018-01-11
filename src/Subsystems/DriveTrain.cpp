151f463cfa523eeb695b5f15aed9d6dcae7c3696
Christopher Rae <crae.main@gmail.com>
2018-01-10 17:10:08 -0700

    Added Joystick Control as default drivetrain command.

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -1,6 +1,8 @@
 #include "DriveTrain.h"
 #include "../RobotMap.h"
 
+#include "../Commands/ArcadeDriveWithJoysticks.h"
+
 DriveTrain::DriveTrain() : Subsystem("DriveTrain"),
 	driveTrain(DRIVE_LEFT, DRIVE_RIGHT)
 {
@@ -10,6 +12,7 @@ DriveTrain::DriveTrain() : Subsystem("DriveTrain"),
 void DriveTrain::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
 	// SetDefaultCommand(new MySpecialCommand());
+	SetDefaultCommand(new ArcadeDriveWithJoysticks);
 }
 
 void DriveTrain::arcadeDrive(double speed, double turn){
