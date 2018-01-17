2cb8d0f3e86ea3a9010e6138aa39bc4d6b11c2fd
Shawn Sargent <shawnsargent@1726nerds.org>
2018-01-16 17:21:56 -0700

    fixed added semicolon to RobotMap

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -19,5 +19,6 @@ constexpr int RIGHT_TRIG = 3;
 //Drive Values
 constexpr double DEADZONE = 0.025;
 constexpr double INITIALSPEED = 0.1;
-constexpr double FULLSPEED = 1
+constexpr double FULLSPEED = 1;
+
 #endif  // ROBOTMAP_H
