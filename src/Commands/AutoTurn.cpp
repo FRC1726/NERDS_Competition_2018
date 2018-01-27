9e5f7170f1f87d8e450659b5252c0cab93699b1b
Shawn Sargent <shawnsargent@1726nerds.org>
2018-01-27 12:58:02 -0700

    changed the AutoTurn::isfinished to have a threshold of 1

--- src/Commands/AutoTurn.cpp
+++ src/Commands/AutoTurn.cpp
@@ -33,7 +33,7 @@ void AutoTurn::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool AutoTurn::IsFinished() {
-	if (drivetrain.getAngle() == targetAngle)
+	if (drivetrain.getAngle() > targetAngle -  1 && drivetrain.getAngle() < targetAngle + 1)
 		return true;
 	else
 		return false;
