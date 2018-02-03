04ac46660b061d24fcf44c1315e215dd68cd5dff
Omari Hughes <omarihughes@1726nerds.org>
2018-02-02 18:01:07 -0700

    Added Autonomous mode for left placement

---
+++ src/CommandGroups/Left.cpp
@@ -0,0 +1,41 @@
+#include "Left.h"
+
+#include "Subsystems/DriveTrain.h"
+#include "Commands/DriveByDistance.h"
+#include "Commands/TurnByAngle.h"
+
+#include <string>
+#include <driverstation.h>
+
+Left::Left() {
+	// Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.
+	std::string gameData;
+	gameData = DriverStation::GetInstance().GetGameSpecificMessage();
+		if(gameData.at(0) == 'L')
+		{
+			AddSequential(new DriveByDistance(32));
+			AddSequential(new TurnByAngle(90));
+			AddSequential(new DriveByDistance(32));
+			AddSequential(new TurnByAngle(90));
+		} else {
+			AddSequential(new DriveByDistance(32));
+			AddSequential(new TurnByAngle(90));
+			AddSequential(new DriveByDistance(32));
+			AddSequential(new TurnByAngle(90));
+		}
+}
---
+++ src/CommandGroups/Left.h
@@ -0,0 +1,11 @@
+#ifndef Left_H
+#define Left_H
+
+#include <Commands/CommandGroup.h>
+
+class Left : public CommandGroup {
+public:
+	Left();
+};
+
+#endif  // Left_H
